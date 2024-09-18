6603fdd97beb11eac82fa150aa014c0a13c39725
siddhant2037 <siddhanttak2037@gmail.com>
2024-09-18 20:43:49 +0530

    commit message

--- updates.py
+++ updates.py
@@ -1,5 +1,7 @@
 import streamlit as st
 def app():
+    st.image('WhatsApp Image 2024-09-15 at 21.03.35_1abce9cd.jpg', width=600)
+
     st.title("✨Starting from October 2024:-")
     st.video('Diwalioff.mp4', loop=True, autoplay=True)
     st.text("♦️Above offers are only valid from October-November")
@@ -8,3 +10,4 @@ def app():
     st.video('update1.mp4',loop=True,autoplay=None)
     st.text("♦️Above offers are only valid from June-September")
 
+    st.sidebar.markdown("⚙️Site Created By Siddhant....")
